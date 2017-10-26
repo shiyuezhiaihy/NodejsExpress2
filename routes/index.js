97731301c7363f201458c54b10d97f1fda535e02
shiyuezhiaihy <yhu27@horizon.csueastbay.edu>
2017-10-26 11:42:35 -0700

    intial code dump

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'test index' });
+  res.render('index', { title: 'express' });
 });
 
 router.get('/doit', function(req, res, next) {
